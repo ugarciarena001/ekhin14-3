356be9e62e9c7aef0586bc53af4acb3898b25007
ugarciarena001 <ugarciarena001@gmail.com>
2014-06-29 12:20:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express();
 
 app.get('/', function(req, res) {
-    res.send('¡Hola, mundo!');
+    res.send('¡Hello, world!');
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
